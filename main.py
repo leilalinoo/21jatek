72e384362e9bf03f003ba087c0b362f7e944e779
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-01-31 11:02:00 +0100

    parameterezes listaval

---
+++ main.py
@@ -0,0 +1,4 @@
+import metodusok
+
+#print(metodusok.pontokosszege2([20, 2]))
+print(metodusok.eredmeny(metodusok.pontokosszege2([20, 2]), metodusok.pontokosszege2([5, 2])))
--- metodusok.py
+++ metodusok.py
@@ -1,9 +1,29 @@
 # megoldas
-def eredmeny(gPont, jPont):
-    if jPont > 21:
+
+# def pontokosszege(gLapok, jLapok):
+#     gPontok = 0
+#     jPontok = 0
+#
+#     for i in range(len(gLapok)):
+#         gPontok += gLapok[i]
+#
+#     for i in range(len(jLapok)):
+#         jPontok += jLapok[i]
+#
+#     return gPontok, jPontok
+
+def pontokosszege2(lapok):
+    pontok = 0
+    for i in range(len(lapok)):
+        pontok += lapok[i]
+
+    return pontok
+def eredmeny(gPontok, jPontok):
+
+    if jPontok > 21:
         print("játékos vesztett")
 
-    if gPont > 21:
+    if gPontok > 21:
         print("gép vesztett")
 
 # teszt esetek
