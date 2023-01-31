cae236238eb9bd1416eae441d78fe8e97cc99d6c
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-01-31 11:51:47 +0100

    "jatekos_vesztett" teszteset megirva

--- metodusok.py
+++ metodusok.py
@@ -1,30 +1,38 @@
 # megoldas
-
-# def pontokosszege(gLapok, jLapok):
-#     gPontok = 0
-#     jPontok = 0
-#
-#     for i in range(len(gLapok)):
-#         gPontok += gLapok[i]
-#
-#     for i in range(len(jLapok)):
-#         jPontok += jLapok[i]
-#
-#     return gPontok, jPontok
-
-def pontokosszege2(lapok)->int:
-    pontok:int = 0
+def pontokosszege2(lapok: [int]):
+    pontok: int = 0
     for i in range(len(lapok)):
         pontok += lapok[i]
 
     return pontok
-def eredmeny(gPontok:[int], jPontok:[int]):
-    jPontok:int = pontokosszege2(jPontok)
-    gPontok:int = pontokosszege2(gPontok)
-    if jPontok > 21:
-        print("játékos vesztett")
 
-    if gPontok > 21:
-        print("gép vesztett")
+
+def eredmeny(gLapok: [int], jLapok: [int]):
+    jPontok: int = pontokosszege2(jLapok)
+    gPontok: int = pontokosszege2(gLapok)
+    vegeredmeny = ""
+    if jPontok and gPontok > 21:
+        vegeredmeny = "mindenki vesztett"
+    elif gPontok > 21:
+        vegeredmeny = "gép vesztett"
+    elif jPontok > 21:
+        vegeredmeny = "játékos vesztett"
+
+    return vegeredmeny
 
 # teszt esetek
+def tesztesetek():
+    jatekos_vesztett_teszt()
+
+def jatekos_vesztett_teszt():
+    jatekos = [10, 9, 3]
+    gep = [10, 6]
+    vart_eredmeny = "játékos vesztett"
+    kapott_eredmeny = eredmeny(gep,jatekos)
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("a teszt sikeres")
+    else:
+        print("a teszt megbukott")
+
+tesztesetek()
