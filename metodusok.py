8a16360b07ddbce83474be088df4f0ad2ff3f9f7
leilalinoo <115218496+leilalinoo@users.noreply.github.com>
2023-02-02 16:16:41 +0100

    kisebb21 teszteset, nincs kesz

--- metodusok.py
+++ metodusok.py
@@ -26,11 +26,14 @@ def tesztesetek():
     jatekos_vesztett_teszt()
     j_vesztett_2()
     j_veszt_3()
-
+    gep_vesztett_teszt()
+    gep_vesztett2()
+    gep_vesztett3()
+    kisebb21()
 
 def jatekos_vesztett_teszt():
     gep = [10, 6]
-    jatekos = [10, 1]
+    jatekos = [10, 10, 3]
     vart_eredmeny = "játékos vesztett"
     kapott_eredmeny = eredmeny(gep, jatekos)
 
@@ -62,6 +65,65 @@ def j_veszt_3():
         print("3. \"játékos vesztett\" teszt sikeres")
     else:
         print("3. \"játékos vesztett\" teszt megbukott")
+
+
+# •Ha lapok összege > 21, akkor az vesztett•
+# •Aki közelebb van a 21-hez, az nyert•
+# •Döntetlen esetén az nyer, akinek kevesebb a lapja
+
+def gep_vesztett_teszt():
+    gep = [10, 10, 2]
+    jatekos = [10, 1]
+    vart_eredmeny = "gép vesztett"
+    kapott_eredmeny = eredmeny(gep, jatekos)
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("1. \"gép vesztett\" teszt sikeres")
+    else:
+        print("1. \"gép vesztett\" teszt megbukott")
+
+
+def gep_vesztett2():
+    gep = [10, 9]
+    jatekos = [10, 10, 1]
+    vart_eredmeny = "gép vesztett"
+    kapott_eredmeny = eredmeny(gep, jatekos)
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("2. \"gép vesztett\" teszt sikeres")
+    else:
+        print("2. \"gép vesztett\" teszt megbukott")
+
+
+def gep_vesztett3():
+    gep = [10, 10]
+    jatekos = [10, 10, 1]
+    vart_eredmeny = "gép vesztett"
+    kapott_eredmeny = eredmeny(gep, jatekos)
+
+    if kapott_eredmeny == vart_eredmeny:
+        print("3. \"gép vesztett\" teszt sikeres")
+    else:
+        print("3. \"gép vesztett\" teszt megbukott")
+
+
+def kisebb21():
+    gep = [10, 8]
+    jatekos = [10, 1]
+    geppont = 0
+    jatekospont = 0
+
+    for i in range(len(gep)):
+        geppont += gep[i]
+
+    for i in range(len(jatekos)):
+        geppont += jatekos[i]
+
+    if geppont < 21 and jatekospont < 21:
+        if geppont > jatekospont:
+            print("kisebb mint 21 teszt: gep nyert")
+        elif jatekospont > geppont:
+            print("kisebb mint 21 teszt: gep nyert")
 
 
 tesztesetek()
